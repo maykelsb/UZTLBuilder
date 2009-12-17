799193562d87ab4ccddbe1aa182f73a7d16c6230
Maykel Santos Braz <maykelsb@yahoo.com.br>
2009-12-17 03:23:00 +0000

    Separação da persistência do projeto(Projeto) do controlador da janela principal(WinMain); Chamadas ainda não reestruturadas para a nova estrutura no projeto, ainda é necessário realizar esta adaptação e seguir para os testes de funcionamento; Também foi adicionada a classe de persistência de Layers, mas ainda não está implementada.

--- model/Projeto.php
+++ model/Projeto.php
@@ -1,30 +1,61 @@
 <?php
-class Projeto {
+final class Projeto {
 
-  private $mainWindow;
+  private static $projeto = null;
 
-  public function __construct($win) {
-    $this->mainWindow = $win;
-  }
+  const EXTENCAO_ARQUIVO_PROJETO = 'utbp';
+  const DESC_TIPO_ARQUIVO_PROJETO = 'Projeto UZTLBuilder';
 
-  public function carregarProjeto() {
-    return $this;
-  }
+  private $xml;
 
-  public function criarProjeto() {
-    return $this;
-  }
+  private $pathProjeto;
+  public function setPathProjeto($path) { $this->pathProjeto = $path; }
+  public function getPathProjeto() { return $this->pathProjeto; }
 
-  private function __construct($nomeProjeto) {
-    if (is_file($nomeProjeto)) {
-      // -- Carregar projeto
-    } else {
-      // -- Criar projeto
+  private $dataCriacao;
+  public function setDataCriacao($data) { $this->dataCriacao = $data; }
+  public function getDataCriacao() { return $this->dataCriacao; }
+
+  private $larguraTile;
+  private $alturaTile;
+  private $larguraMapa;
+  private $alturaMapa;
+  private $quantidadeLayers;
+  private $corDeFundo;
+  private $layers = array();
+
+  private function __construct() { }
+
+  public static function criarProjeto($path) {
+    // -- Criação do diretório de trabalho
+    if (!mkdir($path)) { trigger_error('Não foi possível criar o diretório do projeto.', E_USER_ERROR); }
+    self::$projeto = new Projeto();
+    self::$projeto->setPathProjeto($path);
+    self::$projeto->setDataCriacao(date('Ymd'));
+    // -- Tenta salvar o arquivo do projeto, se não conseguir faz rollback da criação do projeto.
+    if (!self::$projeto->salvarProjeto()) {
+      trigger_error('Não foi possível criar o projeto.', E_USER_ERROR);
+      rmdir($path);
+      self::$projeto = null;
     }
+    return self::$projeto;
   }
 
-  private function 
-  
-  
+  public static function abrirProjeto() {
+    return self::$project;
+  }
+
+  public function salvarProjeto() {
+    $file = $this->pathProjeto . '.' . self::EXTENCAO_ARQUIVO_PROJETO;
+    if (!file_exists($file)) { // -- Novo Projeto
+      $this->xml = new DomDocument('1.0', 'iso-8859-1');
+      $elmRoot = $this->xml->appendChild(new DomElement('projeto'));
+      $elmRoot->setAttributeNode(new DOMAttr('path', $this->pathProjeto));
+      $elmRoot->setAttributeNode(new DOMAttr('criacao', $this->dataCriacao));
+    } else { // -- Projeto Existente
+      
+    }
+    return (false !== file_put_contents($this->xml->saveXML()));
+  }
 }
 ?>
