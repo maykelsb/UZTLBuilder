a7e2bd7a7bedcf7e554a21d4211fb46d18fe5a84
Maykel Santos Braz <maykelsb@yahoo.com.br>
2010-04-24 11:36:34 +0000

    Implementação da seleção de tiles e inserção, substituição e remoção de tiles da área de trabalho;

--- controller/WinMain.php
+++ controller/WinMain.php
@@ -52,9 +52,10 @@ final class WinMain extends Window {
   */
   private $projeto = null;
 
-  private $tilesSelecionados = array(
-    'principal' => null,
-    'secundário' => null);
+  /**
+  *
+  */
+  private $sNomeTile;
 
   /**
   * Define funcionalidades básicas para uma caixa de diálogo de manipulação de arquivo.
@@ -110,6 +111,10 @@ final class WinMain extends Window {
     $pathSelecionado = $this->dlgArquivos('Abrir projeto', Gtk::FILE_CHOOSER_ACTION_OPEN);
     if (!is_null($pathSelecionado)) {
       $this->projeto = Projeto::abrirProjeto($pathSelecionado);
+      // -- Após criar o projeto, habilite os botões de configuração e de salvar
+      $this->tbtnSalvar->set_sensitive(true);
+      $this->tbtnConfigurar->set_sensitive(true);
+      $this->tbtnExportar->set_sensitive(true);
       $this->atualizarFormulario();
     }
   }
@@ -141,6 +146,8 @@ final class WinMain extends Window {
       // -- Cópia do tileset para a pasta de trabalho do projeto
       $this->projeto->copiarTileset();
       $this->projeto->salvarProjeto();
+      // -- Atualizando a interface
+      $this->atualizarFormulario();
     }
     $dlg->destroy();
   }
@@ -166,14 +173,6 @@ final class WinMain extends Window {
   }
 
   /**
-  * Atualiza o título da janela principal com o caminho do projeto.
-  */
-  //private function atualizarTitulo() {
-    // -- Atualiza o título da janela principal
-    //$this->WinMain->set_title($this->tituloJanela . ' [' . $this->projeto->pathProjeto . ']');
-  //}
-
-  /**
   * Atualiza a cor de fundo da área de trabalho e da área do tileset.
   */
   private function atualizarCorDeFundo() {
@@ -189,7 +188,7 @@ final class WinMain extends Window {
   * Carrega o conjunto de tiles do projeto e preenche a área de tilesets dos projeto.
   */
   private function carregarTileset() {
-    if (!empty($this->projeto->pathTileset)) {
+    if ('' != $this->projeto->pathTileset) {
       if (!is_null($this->vwpTileset->get_child())) { $this->vwpTileset->remove($this->vwpTileset->get_child()); }
       // -- Construindo tabela de tiles
       list($larguraTileset, $alturaTileset) = getimagesize($this->projeto->pathTileset);
@@ -197,18 +196,59 @@ final class WinMain extends Window {
         ($alturaTileset / $this->projeto->alturaTile));
       // -- Carregando tiles já cortados
       $pathTile = $this->projeto->pathProjeto . DIRECTORY_SEPARATOR . Projeto::PATH_TILES . DIRECTORY_SEPARATOR;
+      // -- Corde fundo para aplicar nos event-boxes, que não são transparentes
+      $gColor = GdkColor::parse($this->projeto->corDeFundo);
+      // -- Preenchendo a grade com os tiles
       for ($x = 0; $x < ($larguraTileset / $this->projeto->larguraTile); $x++) {
         for ($y = 0; $y < ($alturaTileset / $this->projeto->alturaTile); $y++) {
+          $evb = new GtkEventBox();
+          $evb->set_name(sprintf("%02d-%02d", $x, $y));
+          $evb->connect('button-press-event', array($this, 'selectTile'));
+          $evb->modify_bg(Gtk::STATE_NORMAL, $gColor);
+          $evb->add(GtkImage::new_from_file(sprintf("{$pathTile}%02d-%02d.png", $x, $y)));
           $frmTile = new GtkFrame();
-          $frmTile->set_size_request((int)$this->projeto->larguraTile + 2,
-            (int)$this->projeto->alturaTile + 2);
-          $frmTile->add(GtkImage::new_from_file(sprintf("{$pathTile}%02d-%02d.png", $x, $y)));
+          $frmTile->set_shadow_type(1);
+          $frmTile->set_border_width(0);
+          $frmTile->add($evb);
           $tblTiles->attach($frmTile, $x, $x + 1, $y, $y + 1,
             Gtk::EXPAND + Gtk::FILL, Gtk::EXPAND + Gtk::FILL, 0, 0);
         }
       }
       $this->vwpTileset->add($tblTiles);
     }
+  }
+
+  /**
+  *
+  */
+  public function selectTile($widget) { $this->sNomeTile = $widget->get_name(); }
+
+  /**
+  *
+  */
+  public function insertTile($widget, $event) {
+    switch ($event->button) {
+    case 1: // -- Inserir/Sobrescrever tile
+      if ('' != $this->sNomeTile) {
+        $imgTile = GtkImage::new_from_file($this->projeto->pathProjeto
+                                           . DIRECTORY_SEPARATOR . Projeto::PATH_TILES
+                                           . DIRECTORY_SEPARATOR . $this->sNomeTile . '.png');
+        $imgTile->set_name($this->sNomeTile);
+        // -- Verificando se o eventbox já não tem uma imagem como filho
+        $evbChild = $widget->get_child();
+        if (null != $evbChild) { $widget->remove($evbChild); }
+        unset($evbChild);
+        $widget->add($imgTile);
+      }
+      break;
+    case 3: // -- Apagar tile
+      $evbChild = $widget->get_child();
+      if (null != $evbChild) { $widget->remove($evbChild); }
+      unset($evbChild);
+      break;
+    }
+    // -- Atualizando o widget para que exiba as mudanças ocorridas em seu conteúdo
+    $widget->show_all();
   }
 
   /**
@@ -244,24 +284,38 @@ final class WinMain extends Window {
         // -- Criando a tabela de tiles da layer com tamanho suficiente para o frame que abrigará o tile
         $tblLayer = new GtkTable((int)$this->projeto->larguraMapa * ((int)$this->projeto->larguraTile + 2),
           (int)$this->projeto->alturaMapa * ((int)$this->projeto->alturaTile + 2));
+        // -- Corde fundo para aplicar nos event-boxes, que não são transparentes
+        $gColor = GdkColor::parse($this->projeto->corDeFundo);
         foreach ($layer as $col => $linhaTiles) {
           foreach ($linhaTiles as $row => $tile) {
+            $evb = new GtkEventBox();
+            $evb->connect('button-press-event', array($this, 'insertTile'));
+            $evb->modify_bg(Gtk::STATE_NORMAL, $gColor);
+
             $frmTile = new GtkFrame();
-            $frmTile->set_size_request((int)$this->projeto->larguraTile + 2,
-              (int)$this->projeto->alturaTile + 2);
-            if (!is_null($tile)) { // -- Adicionando um tile previamente selecionado
-              $frmTile->add(GtkImage::new_from_file("{$pathTile}{$tile}.png"));
-              $tblLayer->attach($frmTile, $col, $col + 1, $row, $row + 1,
-                Gtk::EXPAND + Gtk::FILL, Gtk::EXPAND + Gtk::FILL, 0, 0);
-            } else { // -- Criando uma posição vazia para, posteriormente, adicionar um novo tile
-              $tblLayer->attach($frmTile, $col, $col + 1, $row, $row + 1,
-                Gtk::EXPAND + Gtk::FILL, Gtk::EXPAND + Gtk::FILL, 0, 0);
+            $frmTile->set_shadow_type(1);
+            $frmTile->set_size_request((int)$this->projeto->larguraTile + 4,
+              (int)$this->projeto->alturaTile + 4);
+            $frmTile->add($evb);
+
+            if ('' != $tile) { // -- Adicionando um tile previamente selecionado
+              var_dump("{$pathTile}{$tile}.png");
+              $imgTile = GtkImage::new_from_file("{$pathTile}{$tile}.png");
+              $imgTile->set_name($tile);
+              $evb->add($imgTile);
             }
+            // -- Anexando o tile à tabela da área de trabalho
+            $tblLayer->attach($frmTile, $col, $col + 1, $row, $row + 1,
+              Gtk::EXPAND + Gtk::FILL, Gtk::EXPAND + Gtk::FILL, 0, 0);
           }
         }
         $this->fxdAreaTrabalho->put($tblLayer, 0, 0);
       }
     }
+  }
+
+  private function dumpLayers() {
+    
   }
 }
 ?>
